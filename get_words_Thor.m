c493ae37a775349632aa877033516a97c8e5233e
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-11-29 19:05:41 -0800

    Delete get_words_Thor.m

--- get_words_Thor.m
+++
@@ -1,7 +0,0 @@
-function data=get_words_Thor(Thor)
-% data=GETWORDS(hero)
-% This function gets the words from a .txt file and stores them as an array
-FID =fopen('Thor.txt');% Open the file
-data= textscan(FID,'%s');%scan through the file for each word;
-fclose(FID); %close the file
-end
