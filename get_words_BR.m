e7c91256d3e0ca522b8c29c5ccae9000d34c2023
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-01 15:31:21 -0800

    Add files via upload

---
+++ get_words_BR.m
@@ -0,0 +1,30 @@
+function data=get_words_BR(S)
+% data=GETWORDS(hero)
+% This function gets the words from a .txt file and stores them as an array
+S=["Thor","Hulk","Eyepatch","Blackwidow","Ironman","CaptinAmerica"];
+if S=="Thor"
+    FID=fopen("Thor.txt");
+    data= textscan(FID,'%s');%scan through the file for each word;
+    fclose(FID); %close the file
+elseif S=="Hulk"
+    FID=fopen("Bruce_Banner.txt");
+    data= textscan(FID,'%s');%scan through the file for each word;
+    fclose(FID);
+elseif S=="Eyepatch"
+    FID=fopen("Nick_Fury.txt");
+    data= textscan(FID,'%s')%scan through the file for each word;
+    fclose(FID);
+elseif S=="Blackwidow"
+    FID=fopen("Natasha_Romanoff.txt");
+    data= textscan(FID,'%s');%scan through the file for each word;
+    fclose(FID);
+elseif S=="Ironman"
+    FID=fopen("Tony_Stark.txt");
+    data= textscan(FID,'%s');%scan through the file for each word;
+    fclose(FID);
+else S=="CaptinAmerica"
+    FID=fopen("Steve_Rogers.txt");
+    data= textscan(FID,'%s')%scan through the file for each word;
+    fclose(FID);
+end
+end
