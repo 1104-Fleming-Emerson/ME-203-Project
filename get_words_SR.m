20cbfa86684c19e8bf1a960996325b777c4c52aa
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-01 15:30:47 -0800

    Delete get_words_SR.m

--- get_words_SR.m
+++
@@ -1,7 +0,0 @@
-function data=get_words_SR(Cap)
-% data=GETWORDS(hero)
-% This function gets the words from a .txt file and stores them as an array
-FID =fopen('Steve_Rogers.txt');% Open the file
-data= textscan(FID,'%s');%scan through the file for each word;
-fclose(FID); %close the file
-end
