23efb8338ed55621163ffe2f3e834933e4e84396
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-01 17:11:07 -0800

    Add files via upload

--- Project_Question_3.m
+++ Project_Question_3.m
@@ -1,15 +1,8 @@
 % Brandon Ramirez Lopez
 % Question Three
 clc,clear all,format compact
-S="Thor"
+S="Thor"'
 d=get_words_BR(S);
-d{1};
-data=d{1};
-for i=1:length(data)
-    data(i)=erase(data,".") %Removes "." from every string
-    data(i)=erase(data,",") %Removes "," from every string
-    data(i)=erase(data,"?") %Removes "?" from every string
-    data(i)=erase(data,"!") %Removes "!" from every string
-    data(i)=lower(data)%lowercases every string
-end
+data=d'
+Output=remove_delim_BR(data)
 
