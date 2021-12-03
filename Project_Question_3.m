4f280cfa5e8122bd11e0f70739351f19f0503595
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-02 23:29:30 -0800

    Add files via upload

--- Project_Question_3.m
+++ Project_Question_3.m
@@ -1,8 +1,8 @@
 % Brandon Ramirez Lopez
 % Question Three
 clc,clear all,format compact
-S="Thor";
-d=get_words_BR(S);
+Hero="Thor";
+d=get_words_BR(Hero);
 data=d;
 Output=remove_delim_BR(data);
 [data_filtered,L]=clean_words_BR(Output);
