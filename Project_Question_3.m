ee53f5080d0676c069b9504684f58afd94ceba94
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-02 23:17:10 -0800

    Add files via upload

--- Project_Question_3.m
+++ Project_Question_3.m
@@ -5,5 +5,7 @@ S="Thor";
 d=get_words_BR(S);
 data=d;
 Output=remove_delim_BR(data);
-data_filtered=clean_words_BR(Output);
-[barx,count]=categorizes_words_BR(data_filtered)
+[data_filtered,L]=clean_words_BR(Output);
+[barx,count]=categorizes_words_BR(data_filtered);
+[TOP,subcount]=Top_N (count,barx,L)
+
