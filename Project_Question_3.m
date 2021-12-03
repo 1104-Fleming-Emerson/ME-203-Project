237881066b1ba2632c1d480177ee7a077621ec2c
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-03 13:42:52 -0800

    Add files via upload

--- Project_Question_3.m
+++ Project_Question_3.m
@@ -1,11 +1,7 @@
 % Brandon Ramirez Lopez
 % Question Three
 clc,clear all,format compact
-Hero="Thor";
-d=get_words_BR(Hero);
-data=d;
-Output=remove_delim_BR(data);
-[data_filtered,L]=clean_words_BR(Output);
-[barx,count]=categorizes_words_BR(data_filtered);
-[TOP,subcount]=Top_N (count,barx,L)
+Hero = input('Choose Hero name:\n');
+N=input('Choose the number of words that you want to see:\n');
+[Top,Subcount] = combinefunctions_BR(Hero,N)
 
