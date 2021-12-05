56a34018a316794d4b72f64af7a0c605d712fe5d
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-05 15:46:24 -0800

    Add files via upload

--- get_words_BR.m
+++ get_words_BR.m
@@ -21,6 +21,10 @@ elseif S=="Iron man"|S=="Tony Stark"
     FID=fopen("Tony_Stark.txt");
     data= textscan(FID,'%s');%scan through the file for each word;
     fclose(FID);
+elseif S=="Hawk eye"|S=="Clint Barton"
+    FID=fopen("Clint_Barton.txt");
+    data= textscan(FID,'%s');%scan through the file for each word;
+    fclose(FID);
 else S== "Captin America"|S=="Steve Roger"
     FID=fopen("Steve_Rogers.txt");
     data= textscan(FID,'%s');%scan through the file for each word;
