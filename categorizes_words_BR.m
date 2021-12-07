914154a1ab76f9326d7bbb5a4e743c0afc95aa0d
Brandon3298 <94567135+Brandon3298@users.noreply.github.com>
2021-12-06 18:25:23 -0800

    Update categorizes_words_BR.m

--- categorizes_words_BR.m
+++ categorizes_words_BR.m
@@ -1,4 +1,6 @@
 function [barx,count]=categorizes_words_BR(data_filtered)
-c =sort(data_filtered);
-[count,barx]=groupcounts(c);
+%categorizes_words_BR(data_filtered)
+% This function categorizes the words in alphabetical order and then sorts them by most common words and the number of times they appear
+c =sort(data_filtered); % Puts them in alphabetical order
+[count,barx]=groupcounts(c);% Puts them in order from most common word and the number of times they appear
 end
